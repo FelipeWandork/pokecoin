a26da2fb5a028137492e679502403dc54d250aa5
Felipe Wandork <wandork@gmail.com>
2022-04-20 18:07:54 -0300

    Correção de rotas

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :wallets
   get 'historic/write'
   get 'historic/read'
-  root 'pokemons'
+  root 'pokemons/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
